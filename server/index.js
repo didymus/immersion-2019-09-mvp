1509da19e201d71dff70106315644943ed977177
thomasmudge <alephcult@gmail.com>
2019-10-22 21:33:53 -0500

    Staging for push

--- server/index.js
+++ server/index.js
@@ -20,25 +20,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // https://api.shodan.io/tools/httpheaders?key={API_KEY}
 
 const ipSearch = axios.get('https://api.shodan.io/shodan/host/206.82.85.197?key=7FGvLUBX0p9z5ic3t1txmqdycsKhNIh4')
+  .then((response) => { // res.send(req.body.vulns)
+    debugger;
+ response.data.data.data.data.data.data;
+// 
+}).catch((error) => {
+  console.log('Error: ', error);
+});
   //console.log(req.body); // 206.82.85.197
-  
   // res -> call db function
-  
   // ip search end point:
   // https://api.shodan.io/shodan/host/{ip}?key={API_KEY}
-
   // input: will take ips separated by commas as input
   // output: will start the process of putting the vulns into db
 
-const dnsResolve = axios.get('https://api.shodan.io/dns/resolve?hostnames={hostnames}&key={YOUR_API_KEY}')
+// const dnsResolve = axios.get('https://api.shodan.io/dns/resolve?hostnames={hostnames}&key={YOUR_API_KEY}')
+// .then()
+// .catch((error) => {
+//   console.log('Error: ', error);
+// });
 // takes the information parsed by `body-parser` residing in `req.body` (find vulns, docs loc in response)  
 //res.send('hello world');
   // express 'GET':
   // dns resolve end point:
   // https://api.shodan.io/dns/resolve?hostnames={hostnames}&key={YOUR_API_KEY}
-  
   // scrub the inputs for XSS and SQLi eventually
-
   // input: will take domain-name(s) (can be separated by commas)
   
 app.get('/vulns', (req, res) => {
